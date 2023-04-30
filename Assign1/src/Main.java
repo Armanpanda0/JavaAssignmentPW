22e24f828454cbb2eca9736f80d239419598fb90
Arman Panda <112076184+Armanpanda0@users.noreply.github.com>
2023-04-30 12:43:53 +0530

    23rd oct

---
+++ Assign1/src/Main.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+
+/*public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Your !st number: ");
+        int a=sc.nextInt();
+
+        System.out.println("Enter Your Second Number");
+        int b=sc.nextInt();
+
+        int c=a+b;
+
+        System.out.println("Sum is: " + c);
+    }
+}*/
+// Q-2 Find the Ascii Value Of the Alphabate.
+/*public class Main{
+    public static void main(String[] args) {
+                int x='A';
+                System.out.println("Ascii Value of the A is: "+x);
+
+    }
+}*/
+
+
+
+
+//Q-4 Area Of a Rectangle
+
+/*public class Main  {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Length of the Rectangle: ");
+        int x=sc.nextInt();
+
+        System.out.println("Bredth of the Rectangle is:");
+        int y= sc.nextInt();
+
+        System.out.println("Area of the Rectangle is: " + (x*y));
+    }
+}
+ */
+
+// Calculate the cube of the number
+/*public class Main {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+
+        System.out.println("Enter the Number: ");
+        int a=sc.nextInt();
+
+        System.out.println("Cube of is "+ (a*a*a));
+    }
+}
+
+ */
+
+// Swap Two Number With the help of the #rd variable
+
+/*public class Main {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+
+        System.out.println("Inpt the 1st Number: ");
+        int x=sc.nextInt();
+
+        System.out.println("Input 2nd Number: ");
+        int y=sc.nextInt();
+        int z;
+        System.out.println("Before the swap x is: "+x);
+        System.out.println("Before swap y is: "+ y);
+        z=x;
+        x=y;
+        y=z;
+        System.out.println("After swap x is "+ x);
+        System.out.println("After swap y is: "+y);
+    }
+}
+
+ */
